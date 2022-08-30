6843c566ac9479845090d6d8ac6279503be5e67d
LuigiLanza <luigi.lanza@publicissapient.com>
2022-08-30 18:04:07 +0200

    Pagina resource + Link component

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Layout from 'components/Layout';
 import ResourceHighlight from 'components/ResourceHighlight';
 import Newsletter from 'components/Newsletter';
 import ResourceList from 'components/ResourceList';
+import { useEffect } from 'react';
+
 
 function Home({resources}) {
+
   return (
     <Layout>
       <ResourceHighlight resources={resources.slice(0, 2)} />
@@ -14,7 +17,7 @@ function Home({resources}) {
 }
 
 export async function getServerSideProps(){
-  const resData = await fetch("http://localhost:3000/api/resources");
+  const resData = await fetch("http://localhost:3001/api/resources");
   const data = await resData.json();
 
   return{
